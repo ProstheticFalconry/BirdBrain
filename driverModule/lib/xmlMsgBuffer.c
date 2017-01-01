c6716d33830032011a28725e979831b6b07d1053
Demo User <esanu@ualberta.ca>
2017-01-01 03:33:26 +0000

    just saving progress

--- driverModule/lib/xmlMsgBuffer.c
+++ driverModule/lib/xmlMsgBuffer.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <xmlMsgBuffer.h>
+#include <xmlBuffer.h>
 #include <libxml/xmlwriter.h>
 #include <libxml/xmlreader.h>
+
+#ifdef SLAB_H
+	char *ft_strdup(char *src)
+	{
+    		char *str;
+    		char *p;
+    		int len = 0;
+
+    		while (src[len])
+        		len++;
+    		str = malloc(len + 1);
+   	 	p = str;
+    		while (*src)
+        	*p++ = *src++;
+    		*p = '\0';
+    		return str;
+	}
+#endif
 
 XmlBuffer *getXmlMsgBuffer(unsigned int msgCountMax, unsigned int msgSizeMax){
 	XmlMsgBuffer *buf=(XmlBuffer *)malloc(sizeof(XmlBuffer));
